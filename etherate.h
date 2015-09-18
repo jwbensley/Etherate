37ccd75fc690e5fba853104e823a8d9dfb42a0ae
jwbensley <jwbensley@gmail.com>
2015-09-18 14:41:01 +0100

    Updated to beta 0.6

--- etherate.h
+++ etherate.h
@@ -37,202 +37,179 @@
  ************************************************************* GLOBAL CONSTANTS
  */
 
+const char VERSION[20] = "0.6.beta 2015-09";
 
-const string version = "0.5.beta 2015-07";
-
-// Maximum frame size on the wire (payload + 22 octets for QinQ headers)
-// Etherate will support, this is hard coded here because we have to allocate
+// Maximum frame size on the wire (payload + all headers headers) Etherate
+// will support, this is hard coded here because we have to allocate
 // send and receive buffers. 
-const int F_SIZE_MAX = 10000;
-
-// Default frame payload size in bytes
-const int F_SIZE_DEF = 1500;
-
-// Default duration in seconds
-const long F_DURATION_DEF = 30;
-
-// Default total number of frames to transmit
-const long F_COUNT_DEF = 0;
-
-// Default amount of data to transmit in bytes
-const long F_BYTES_DEF = 0;
-
-// Default max speed in bytes, 0 == no limit
-const long B_TX_SPEED_MAX_DEF = 0;
-
-// Default PCP value
-const short PCP_DEF = 0;
-
-// Default VLAN ID
-const short VLAN_ID_DEF = 0;
-
-// Default QinQ VLAN ID
-const short QINQ_ID_DEF = 0;
-
-// Default QinQ PCP value
-const short QINQ_PCP_DEF = 0;
-
-// Default frame headers length
-const int HEADERS_LEN_DEF = 14;
-
-// Default ETHERTYPE (0x0800 == IPv4)
-const int ETHERTYPE_DEF = 2048;
-
-// Default interface index number
-const int IF_INDEX_DEF = -1;
+const unsigned int       F_SIZE_MAX = 10000;
+const unsigned int       F_SIZE_DEF = 1500;      // Default frame payload size in bytes
+const unsigned long long F_DURATION_DEF = 30;    // Default test duration in seconds
+const unsigned long      F_COUNT_DEF = 0;        // Default total number of frames to transmit
+const unsigned long      F_BYTES_DEF = 0;        // Default amount of data to transmit in bytes
+const unsigned long      B_TX_SPEED_MAX_DEF = 0; // Default max speed in bytes, 0 == no limit
+const unsigned short     PCP_DEF = 0;            // Default PCP value
+const unsigned short     VLAN_ID_DEF = 0;        // Default VLAN ID
+const unsigned short     QINQ_ID_DEF = 0;        // Default QinQ VLAN ID
+const unsigned short     QINQ_PCP_DEF = 0;       // Default QinQ PCP value
+const unsigned int       HEADERS_LEN_DEF = 14;   // Default frame headers length
+const unsigned short     ETHERTYPE_DEF = 2048;   // Default ETHERTYPE (0x0800 == IPv4)
+const unsigned int       IF_INDEX_DEF = -1;      // Default interface index number
 
 
-/*
- ************************************************************* GLOBAL CONSTANTS
- */
+#define TYPE_APPLICATION         1
+#define TYPE_APPLICATION_SUB_TLV 11
+#define VALUE_DYINGGASP          101
+#define VALUE_DUMMY              102
 
+#define TYPE_BROADCAST        2
+#define VALUE_PRESENCE        21
 
+#define TYPE_SETTING          3
+#define VALUE_SETTING_SUB_TLV 30
+#define VALUE_SETTING_END     31
+#define TYPE_ETHERTYPE        301
+#define TYPE_FRAMESIZE        302
+#define TYPE_DURATION         303
+#define TYPE_FRAMECOUNT       304
+#define TYPE_BYTECOUNT        305
+#define TYPE_MAXSPEED         306
+#define TYPE_VLANPCP          307
+#define TYPE_QINQPCP          308
+#define TYPE_ACKMODE          309
+#define TYPE_MTUTEST          310
+#define TYPE_MTUMIN           311
+#define TYPE_MTUMAX           312
+
+#define TYPE_TESTFRAME        4
+#define VALUE_TEST_SUB_TLV    40
+#define VALUE_DELAY_END       41
+#define TYPE_FRAMEINDEX       401
+#define TYPE_ACKINDEX         402
+#define TYPE_DELAY            403
+#define TYPE_DELAY1           404
+#define TYPE_DELAY2           405
 
 
 /*
  ************************************************************* GLOBAL VARIABLES
  */
 
-// General application parameters
-struct APP_PARAMS
+struct APP_PARAMS              // General application parameters
 {
 
-    bool TX_MODE; // Default mode is TX
-    bool TX_SYNC; // Sync settings between hosts by default
-
-    time_t TS_NOW;     // Time type for holding the current date and time
-    tm* TM_LOCAL;      // Time struct for breaking down the above time type
+    char   TX_MODE;            // Default mode is TX
+    char   TX_SYNC;            // Default is to Sync settings between hosts
+    char   TX_DELAY;
+    time_t TS_NOW;             // Current date and time
+    tm*    TM_LOCAL;           // For breaking down the above time type
 
 };
 
-/*
- * These are the minimun declarations required to send a frame; 
- * They have been moved into this global variable space. This is because they
- * are being used by the signal handler function signal_handler() to send the
- * dying gasp frame upon program exit()
- */
 
-/////unsigned char SOURCE_MAC[6];
-/////unsigned char DESTINATION_MAC[6];
-/////int ETHERTYPE;
-/////char* TX_BUFFER;
-/////char* TX_DATA;
-/////unsigned char* TX_ETHERNET_HEADER;
-struct ifreq ethreq;
-/////int ETH_HEADERS_LEN;
-/////int SOCKET_FD;
-/////struct sockaddr_ll socket_address;
-/////char IF_NAME[IFNAMSIZ];
-/////int TX_RET_VAL;
-
-// Frame header settings
-struct FRAME_HEADERS
+struct FRAME_HEADERS           // Frame header settings
 {
-    unsigned char SOURCE_MAC[6];
-    unsigned char DESTINATION_MAC[6];
-    int ETHERTYPE;
-    int LENGTH;
-    short PCP;
-    short VLAN_ID;
-    short QINQ_ID;
-    short QINQ_PCP;
-    char* RX_BUFFER;
-    char* RX_DATA;
-    char* TX_BUFFER;
-    char* TX_DATA;
+
+    unsigned char       SOURCE_MAC[6];
+    unsigned char       DESTINATION_MAC[6];
+    unsigned short      ETHERTYPE;
+    unsigned short      LENGTH;
+    unsigned short      PCP;
+    unsigned short      VLAN_ID;
+    unsigned short      QINQ_ID;
+    unsigned short      QINQ_PCP;
+    char*               RX_BUFFER;
+    char*               RX_DATA;
+    char*               TX_BUFFER;
+    char*               TX_DATA;
+    unsigned int        TLV_SIZE;
+    unsigned short*     RX_TLV_TYPE;
+    unsigned long*      RX_TLV_VALUE;
+    unsigned int        SUB_TLV_SIZE;
+    unsigned short*     RX_SUB_TLV_TYPE;
+    unsigned long long* RX_SUB_TLV_VALUE;
 };
 
-// A global pointer is needed for the SIGINT catch, signal_handler()
-struct FRAME_HEADERS *pFRAME_HEADERS;
 
-
-// Settings for the physical interface used for testing
-struct TEST_INTERFACE
+struct TEST_INTERFACE          // Settings for the physical test interface
 {
-	int IF_INDEX;
-    char IF_NAME[IFNAMSIZ];
+
+	int    IF_INDEX;
+    char   IF_NAME[IFNAMSIZ];
     struct sockaddr_ll SOCKET_ADDRESS;
-    
-    int SOCKET_FD;             // Used for sending frames
-    fd_set FD_READS;           // A set of socket file descriptors for polling with select()
-    int SOCKET_FD_COUNT;       // FD count and ret val for polling with select()
-    int SELECT_RET_VAL;
-
-    struct timeval TV_SELECT_DELAY; // Elapsed time struct for polling the socket file descriptor
+    int    SOCKET_FD;                   // Used for sending frames
+    fd_set FD_READS;                    // Socket file descriptors for polling with select()
+    int    SOCKET_FD_COUNT;             // FD count and ret val for polling with select()
+    int    SELECT_RET_VAL;
+    struct timeval TV_SELECT_DELAY;     // Elapsed time struct for polling the socket FD
 
 };
 
-// A global pointer is needed for the SIGINT catch, signal_handler()
-struct TEST_INTERFACE *pTEST_INTERFACE;
 
-
-// Gerneral testing parameters
-struct TEST_PARAMS
+struct TEST_PARAMS             // Gerneral testing parameters
 {
 
-    int F_SIZE;                // Frame payload in bytes
-    int F_SIZE_TOTAL;          // Total frame size including headers
-    long long F_DURATION;      // Maximum duration in seconds
-    long long F_COUNT;         // Maximum number of frames to send
-    long long F_BYTES;         // Maximum amount of data to transmit in bytes
-    long B_TX_SPEED_MAX;       // Maximum speed to transmit at (max bytes per second)
-    long B_TX_SPEED_PREV;      // Transmission speed for the previous second
-    timespec TS_TX_LIMIT;      // Timer used for rate limiting the TX host
-    long long S_ELAPSED;       // Seconds the test has been running
-
-    long long F_TX_COUNT;      // Total number of frames transmitted
-    long long F_TX_COUNT_PREV; // Total number of frames sent up to one second ago
-    long long B_TX;            // Total number of bytes transmitted
-    long long B_TX_PREV;       // Bytes sent up to one second ago
-    long long F_RX_COUNT;      // Total number of frames received
-    long long F_RX_COUNT_PREV; // Total number of frames received up to one second ago
-    long F_RX_OTHER;           // Number of non test frames received
-    long long B_RX;            // Total number of bytes received
-    long long B_RX_PREV;       // Bytes received up to one second ago
-
-    long long F_INDEX;         // Index of the current test frame sent/received
-    long long F_INDEX_PREV;    // Index of the last test frame sent/received
-    long long F_RX_ONTIME;     // Frames received on time
-    long long F_RX_EARLY;      // Frames received out of order that are early
-    long long F_RX_LATE;       // Frames received out of order that are late        
-    
-    float B_SPEED;             // Current speed
-
-    bool F_ACK;                // Testing in ACK mode during transmition
-    bool F_WAITING_ACK;        // Test is waiting for a frame to be ACK'ed
-    timespec TS_ACK_TIMEOUT;   // Timeout to wait for a frame ACK
-    
-    timespec TS_CURRENT_TIME;  // Two timers for timing a test and calculating stats
-    timespec TS_ELAPSED_TIME;
+    unsigned short     F_SIZE;          // Frame payload in bytes
+    unsigned short     F_SIZE_TOTAL;    // Total frame size including headers
+    unsigned long long F_DURATION;      // Maximum duration in seconds
+    unsigned long long F_COUNT;         // Maximum number of frames to send
+    unsigned long long F_BYTES;         // Maximum amount of data to transmit in bytes
+    unsigned long      B_TX_SPEED_MAX;  // Maximum transmit speed in bytes per second
+    unsigned long      B_TX_SPEED_PREV; // Transmission speed for the previous second
+    timespec           TS_TX_LIMIT;     // Timer used for rate limiting the TX host
+    unsigned long long S_ELAPSED;       // Seconds the test has been running
+    unsigned long long F_TX_COUNT;      // Total number of frames transmitted
+    unsigned long long F_TX_COUNT_PREV; // Total number of frames sent one second ago
+    unsigned long long B_TX;            // Total number of bytes transmitted
+    unsigned long long B_TX_PREV;       // Bytes sent up to one second ago
+    unsigned long long F_RX_COUNT;      // Total number of frames received
+    unsigned long long F_RX_COUNT_PREV; // Total number of frames received one second ago
+    unsigned long long F_RX_OTHER;      // Number of non test frames received
+    unsigned long long B_RX;            // Total number of bytes received
+    unsigned long long B_RX_PREV;       // Bytes received one second ago
+    unsigned long long F_INDEX_PREV;    // Index of the last test frame sent/received
+    unsigned long long F_RX_ONTIME;     // Frames received on time
+    unsigned long long F_RX_EARLY;      // Frames received out of order that are early
+    unsigned long long F_RX_LATE;       // Frames received out of order that are late        
+    double             B_SPEED;         // Current speed
+    double             B_SPEED_MAX;     // The maximum speed achieved during the test
+    long double        B_SPEED_AVG;     // The average speed achieved during the test
+    char               F_ACK;           // Testing in ACK mode during transmition
+    char               F_WAITING_ACK;   // Test is waiting for a frame to be ACK'ed
+    timespec           TS_CURRENT_TIME; // Two timers for timing a test and calculating stats
+    timespec           TS_ELAPSED_TIME;
 
 };
 
 
-// Settings specific to the MTU sweet test
-struct MTU_TEST {
+struct MTU_TEST {              // Settings specific to the MTU sweep test
 
-    bool ENABLED;              // Enable the MTU sweep test mode
-    
-    int MTU_TX_MIN;            // Default minmum MTU size
-    int MTU_TX_MAX;            // Default maximum MTU size
+    char          ENABLED;              // Enable the MTU sweep test mode
+    unsigned int  MTU_TX_MIN;           // Default minmum MTU size
+    unsigned int  MTU_TX_MAX;           // Default maximum MTU size
 
 };
 
 
-// Settings specific to the quality measurement test
-struct QM_TEST {
+struct QM_TEST {               // Settings specific to the quality measurement test
 
-    bool ENABLED;              // Enable the quality measurement tests
-
-    int INTERVAL;              // Default echo interval in milliseconds
-    long INTERVAL_SEC;
-    long INTERVAL_NSEC;
-    int TIMEOUT;               // Default timeout in milliseconds
-    long TIMEOUT_NSEC;
-    long TIMEOUT_SEC;
-
-    int DELAY_TEST_COUNT;      // Number of one way delay measurements to account for interrupt coalescing
-    double *pDELAY_RESULTS;    // Store Tx to Rx test results
-    timespec TS_RTT;           // Timespec used for calculating delay/RTT
+    char          ENABLED;              // Enable the quality measurement tests
+    unsigned int  INTERVAL;             // Default echo interval in milliseconds
+    unsigned long INTERVAL_SEC;
+    unsigned long INTERVAL_NSEC;
+    unsigned int  TIMEOUT;              // Default timeout in milliseconds
+    unsigned long TIMEOUT_NSEC;
+    unsigned long TIMEOUT_SEC;
+    unsigned int  DELAY_TEST_COUNT;     // Number of one way delay measurements
+    double        *pDELAY_RESULTS;      // Store Tx to Rx test results
+    timespec      TS_RTT;               // Timespec used for calculating delay/RTT
 
 };
+
+
+// These need to be global so that signal_handler() can send a dying gasp
+// and the allocated buffers can be free()'d
+struct ifreq ethreq;
+struct QM_TEST *pQM_TEST;
+struct TEST_INTERFACE *pTEST_INTERFACE;
+struct FRAME_HEADERS *pFRAME_HEADERS;
